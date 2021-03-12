57bae72514c2ef7408e86a21745881550a779242
Shilpa <shilpamjoy@gmail.com>
2021-03-12 14:41:03 -0500

    Initial Commit

---
+++ Project5Closures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5Closures
+//
+//  Created by Shilpa Joy on 2021-03-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
